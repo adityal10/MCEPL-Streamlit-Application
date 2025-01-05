1f077b9f5cb005fb0d2b0128d4bec98778c8d0c2
SaiAdityaLakkum <86429416+adityal10@users.noreply.github.com>
2025-01-05 12:06:11 +0530

    Updated host variable

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT", "3306")  # Default MySQL port
+DB_PORT = 3306  # Default MySQL port
 
 
 def main():

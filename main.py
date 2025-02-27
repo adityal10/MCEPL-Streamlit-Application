2c86b99fe70f1c4c810f6f0e00a6bef67673bf93
SaiAdityaLakkum <86429416+adityal10@users.noreply.github.com>
2025-02-27 13:21:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-DB_PORT = 3306  # Default MySQL port
+# DB_PORT = 3306  # Default MySQL port
 
 
 def main():
@@ -33,8 +33,8 @@ def main():
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            database=DB_NAME,
-            port=DB_PORT
+            database=DB_NAME
+            # port=DB_PORT
         )
         # Database manager
         # try:
